63a1e07c1dc98e46a3b32b8921e181aaf3d16e9c
TWR-pixel <sena85088@gmail.com>
2024-11-19 20:28:27 +0300

    Solution structure updated

--- src/Application/TaskManager.Application/User/Common/Email/Code/ICodeGenerator.cs
+++ src/Application/TaskManager.Application/User/Common/Email/Code/ICodeGenerator.cs
@@ -1,6 +1,6 @@
 ﻿namespace TaskManager.Application.User.Common.Email.Code;
 
-public interface ICodeGenerator<in T> where T : class
+public interface ICodeGenerator<in TResult> where TResult : class
 {
     public string GenerateCode(int length);
 }
